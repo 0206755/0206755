9478a125441b66d2b8516e10b6b8624033a77548
0206755 <0206755@rgu.ac.uk>
2016-03-14 10:48:59 +0000

    commit

--- Week7/login.php
+++ Week7/login.php
@@ -18,7 +18,7 @@ if (empty($_POST["username"]) || empty($_POST["password"])) {
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result=mysqli_query($db,$sql);
 
-    if(mysqli_num_rows($result) == 1) {
+    if(mysqli_num_rows($uid) == 1) {
         header ("location: home.php");
     }else {
         echo "Incorrect username or password";
