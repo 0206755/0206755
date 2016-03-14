837cd8ff4a48fc98a6221d0cda59fdd03708580e
0206755 <0206755@rgu.ac.uk>
2016-03-14 10:15:05 +0000

    commit

---
+++ Week7/connect2DB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0206755
+ * Date: 14/03/2016
+ * Time: 09:57
+ */
+
+define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define ('DB_USERNAME', 'b9b4f1f2913587');
+define ('DB_PASSWORD', '81a7c479');
+define ('DB_DATABASE','myDB');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
