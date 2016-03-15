f521548882c7c36e7dcbfd1ab5736741470c58fe
0206755 <0206755@rgu.ac.uk>
2016-03-15 10:14:16 +0000

    commit

---
+++ Movies/display_all_movies.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Display All Movies - Films</title>
+</head>
+<body>
+<h1>Display All Movies</h1>
+<?php
+include("header.html");
+include("connect2DB.php");
+$result = mysql_query("Select * FROM marvelmovies")
+or die(mysql_error());
+
+echo "<table border='1'>";
+echo "<tr> <th>Name</th> <th>Age</th> </tr>";
+// keeps getting the next row until there are no more to get
+while($row = mysql_fetch_array( $result )) {
+    // Print out the contents of each row into a table
+    echo "<tr><td>";
+    echo $row['name'];
+    echo "</td><td>";
+    echo $row['age'];
+    echo "</td></tr>";
+}
+echo "</table>";
+?>
+</body>
+</html>
