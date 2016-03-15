9199b23554b9b320409245f120e489c80601c41a
0206755 <0206755@rgu.ac.uk>
2016-03-15 10:21:29 +0000

    commit

--- Movies/display_all_movies.php
+++ Movies/display_all_movies.php
@@ -9,21 +9,11 @@
 <?php
 include("header.html");
 include("connect2DB.php");
-$result = mysqli_query("Select * FROM marvelmovies")
-or die(mysqli_error());
+$sql = "SELECT * from marvel movies";
 
-echo "<table border='1'>";
-echo "<tr> <th>Name</th> <th>Age</th> </tr>";
-// keeps getting the next row until there are no more to get
-while($row = mysqli_fetch_array( $result )) {
-    // Print out the contents of each row into a table
-    echo "<tr><td>";
-    echo $row['name'];
-    echo "</td><td>";
-    echo $row['age'];
-    echo "</td></tr>";
-}
-echo "</table>";
+$result = mysqli_query($db, $sql);
+$row = mysql_fetch_array($result,
+    MYSQLI_ASSOC);
 ?>
 </body>
 </html>
