6d7bf8361bd0f0ca365905bf2f1c52c95e292848
0206755 <0206755@rgu.ac.uk>
2016-03-15 09:03:02 +0000

    commit

--- Week7/connect2DB.php
+++ Week7/connect2DB.php
@@ -9,7 +9,7 @@
 define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define ('DB_USERNAME', 'b9b4f1f2913587');
 define ('DB_PASSWORD', '81a7c479');
-define ('DB_DATABASE','myDB');
+define ('DB_DATABASE','acsm_3a7bd64b8322cf2');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
