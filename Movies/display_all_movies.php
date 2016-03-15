401fa1f3dbd9a8ca24d180b8503a425a2fb76a94
0206755 <0206755@rgu.ac.uk>
2016-03-15 10:37:34 +0000

    commit

--- Movies/display_all_movies.php
+++ Movies/display_all_movies.php
@@ -15,14 +15,20 @@ $result = mysqli_query($db, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
 echo "<table border='1'>";
-echo "<tr> <th>Name</th> <th>Age</th> </tr>";
+echo "<tr>  </tr>";
 // keeps getting the next row until there are no more to get
 while($row = mysqli_fetch_array( $result )) {
     // Print out the contents of each row into a table
     echo "<tr><td>";
-    echo $row['name'];
+    echo $row['marvelMovieID'];
     echo "</td><td>";
-    echo $row['age'];
+    echo $row['yearReleased'];
+    echo "</td></tr>";
+    echo $row['title'];
+    echo "</td><td>";
+    echo $row['productionStudio'];
+    echo "</td></tr>";
+    echo $row['notes'];
     echo "</td></tr>";
 }
 
