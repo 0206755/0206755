2b76937632dc13431785413098a90a5ad8b07ec3
0206755 <0206755@rgu.ac.uk>
2016-03-16 09:24:28 +0000

    commit

---
+++ Movies/display_all_movies_created_after_2010.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Display All Movies - Films</title>
+</head>
+<body>
+<h1>Display All Movies</h1>
+<?php
+include("header.html");
+include("connect2DB.php");
+$sql = "SELECT * from marvelmovies WHERE yearReleased > '2010'";
+
+$result = mysqli_query($db, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+
+// keeps getting the next row until there are no more to get
+while($row = mysqli_fetch_array( $result )) {
+    // Print out the contents of each row into a table
+
+    echo $row['marvelMovieID'];
+    echo "<br /><br />";
+    echo $row['yearReleased'];
+    echo "<br /><br />";
+    echo $row['title'];
+    echo "<br /><br />";
+    echo $row['productionStudio'];
+    echo "<br /><br />";
+    echo $row['notes'];
+    echo "<br /><br />";
+    echo "<br /><br />";
+
+}
+
+
+
+?>
+</body>
+</html>
