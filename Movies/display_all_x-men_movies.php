3ced9e92aef2595bdc20137924846bb9985e34e7
0206755 <0206755@rgu.ac.uk>
2016-03-21 08:48:49 +0000

    commit

--- Movies/display_all_x-men_movies.php
+++ Movies/display_all_x-men_movies.php
@@ -14,7 +14,7 @@
 <?php
 include("menu.html");
 include("connect2DB.php");
-$sql = "SELECT * from marvelmovies WHERE title LIKE = '%X-Men%'";
+$sql = "SELECT * from marvelmovies WHERE title LIKE = 'X-Men%'";
 
 $result = mysqli_query($db, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
