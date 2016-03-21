e56813ce3732f9b59c689880ecb0ceaab5328a78
0206755 <0206755@rgu.ac.uk>
2016-03-21 09:16:40 +0000

    commit

---
+++ Week8/helloPrinter.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0206755
+ * Date: 21/03/2016
+ * Time: 09:14
+ */
+
+echo "Hello {$planet}";
+
+?>
---
+++ Week8/helloSelector.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0206755
+ * Date: 21/03/2016
+ * Time: 09:11
+ */
+
+<a href="helloPrinter.php?planet=Earth">Earth</a><br />
+<a href="helloPrinter.php?planet=Mars">Mars</a><br />
+<a href="helloPrinter.php?planet=Uranus">Uranus</a><br />
+
+?>
