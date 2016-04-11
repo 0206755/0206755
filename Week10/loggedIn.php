43c3968d455f344da91a23e7d54d68f13f5c441b
0206755 <0206755@rgu.ac.uk>
2016-04-11 10:06:11 +0100

    commit

--- Week10/loggedIn.php
+++ Week10/loggedIn.php
@@ -9,14 +9,14 @@
 session_start();
 $accesslevel = $_COOKIE['access_level_cookie'];
 
-displayAccessLevelnformation($accesslevel);
+displayAccessLevelnformation($accessLevel);
 
 function displayAccessInformation($accesslevel) {
-    if ($accesslevel == "standarduser") {
+    if ($accessLevel == "standarduser") {
         echo "<p style = \background-color:lightgreen\">You are currently logged in as a standard user</p>";
     }
 
-    elseif {$accessLevel == "root"} {
+    elseif ($accessLevel == "root") {
         echo "<p style = \"background-color: red\">You are currently logged in as a root user</p>";
         echo "<p style = \"background-color: red\">You now have access to additional administrative features</p>";
     }
