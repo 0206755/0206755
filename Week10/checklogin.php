867147c9bbaea1aecae80ede19984d1d39c4d724
0206755 <0206755@rgu.ac.uk>
2016-04-11 10:03:04 +0100

    commit

---
+++ Week10/checklogin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0206755
+ * Date: 11/04/2016
+ * Time: 09:55
+ */
+
+$username = $_POST["name"];
+$password = $_POST["password"];
+
+if ($username == "username" && $password == "password"){
+    setcookie('access_level_cookie','standarduser');
+}
+
+header ('Location:loggedIn.php');
+
+?>
