7381eae077099e2d6b331e9f73e1d789efdc2393
0206755 <0206755@rgu.ac.uk>
2016-04-11 09:30:55 +0100

    commit

---
+++ Week10/home.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+<p>Hello
+
+<?php
+    $username =$_GET['username'];
+    echo $username; ?>
+
+</p>
+</body>
+</html>
